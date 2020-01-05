fa76544cd7f45f2c3441120cd68a2641722310e6
zhxwu <zhxwu@hotmail.com>
2020-01-05 16:00:43 +0800

    一个简单的使用lettuce访问redis的例子

---
+++ RedisSample/src/main/java/Main.java
@@ -0,0 +1,16 @@
+import io.lettuce.core.RedisClient;
+import io.lettuce.core.api.StatefulRedisConnection;
+import io.lettuce.core.api.sync.RedisCommands;
+
+public class Main {
+    public static void main(String[] args) {
+        RedisClient redisClient = RedisClient.create("redis://192.168.133.130:6379/0");
+        StatefulRedisConnection<String, String> connection = redisClient.connect();
+        RedisCommands<String, String> syncCommands = connection.sync();
+
+        syncCommands.set("key", "Hello, Redis!");
+
+        connection.close();
+        redisClient.shutdown();
+    }
+}
